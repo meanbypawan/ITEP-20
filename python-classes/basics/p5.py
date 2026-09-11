cf3bfbdf6f4767252ae475795c4fa5417cf10b57
mean <meanstack4@gmail.com>
2026-09-11 14:43:01 +0530

    python basic assignments

---
+++ python-classes/basics/p5.py
@@ -0,0 +1,7 @@
+# input()
+# Typecasting
+a = 10
+b = 20
+c = a + b
+#print("Addition : ",c)
+print(f"Addition : {c}")
---
+++ python-classes/basics/p6.py
@@ -0,0 +1,11 @@
+a = input("Enter 1st value : ")
+b = input("Enter 2nd value : ")
+
+
+#print(f"a : {a} type of a : {type(a)}")
+#print(f"b : {b} type of b : {type(b)}")
+a = int(a)
+b = int(b)
+
+c = a + b
+print(f"Addition : {c}")
